64123f4e10984d735326579f26cac65ff60fa78e
gjspencer22 <gjspencer22@gmail.com>
2021-06-24 02:12:47 -0600

    first commit

---
+++ Employee.js
@@ -0,0 +1,12 @@
+class Employee {
+    constructor(name ='') {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        
+    }
+
+    getName() {
+        return `${this.name}`;
+    }
+}
